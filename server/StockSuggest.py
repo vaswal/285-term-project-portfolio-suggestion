24a8bfc9dbc5e8c884d34ebe5cee33059fdbf80d
Vini Aswal <vijendersingh.aswal@sjsu.edu>
2020-05-15 00:20:44 -0700

    Fixed bug where strategy name was not being returned and refactored code

--- server/StockSuggest.py
+++ server/StockSuggest.py
@@ -1,10 +1,4 @@
-from collections import defaultdict, OrderedDict
-import datetime
-import numpy as np, numpy.random
-import urllib.request as urllib2
-import csv
 import requests
-import json
 
 # API_KEY = 'DSJ9W3ZS7A6RWBJC'
 # API_BASE_CURRENT = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&apikey={}&symbol={}&interval=1min' \
@@ -13,88 +7,90 @@ import json
 
 symbol_map = {}
 
+
 def get_month(stock_symbol):
     session = requests.session()
-    url = "https://financialmodelingprep.com/api/v3/historical-price-full/"+stock_symbol+"?timeseries=30"
+    url = "https://financialmodelingprep.com/api/v3/historical-price-full/" + stock_symbol + "?timeseries=30"
     # print (url)
     response = session.get(url, timeout=15)
-    try: 
+    try:
         stock_data = response.json()
     except ValueError:
-        tempData = { 'error_msg' :'Deserialization Fails.'}
+        tempData = {'error_msg': 'Deserialization Fails.'}
         return tempData
 
     close = float(stock_data['historical'][0]['close'])
     open = float(stock_data['historical'][29]['open'])
     # print("month chnage")
     # print(close-open)
-    return close-open
+    return close - open
+
 
 # get all the stocks and percentages for the selected strategies
 def get_all(strategy_list):
-    stock_percent_list={}
-    response=[]
-    if(len(strategy_list)==1):
+    stock_percent_list = {}
+    response = []
+    if (len(strategy_list) == 1):
         print("get all....")
-        name=strategy_list[0]
-        stock_percent_list=get_stock_list(strategy_list[0])
-        return modify_response(stock_percent_list,name)
+        name = strategy_list[0]
+        stock_percent_list = get_stock_list(strategy_list[0])
+        return modify_response(stock_percent_list, name)
     else:
         for strategy in strategy_list:
             print("get all")
-            name=strategy
-            stock_percent_list=get_stock_list(strategy)
-            response.append(modify_response(stock_percent_list,name))
+            name = strategy
+            stock_percent_list = get_stock_list(strategy)
+            response.append(modify_response(stock_percent_list, name))
         return response
 
 
-def modify_response(stock_percent_list,name):
-    print("name")
-    print(name)
-    sum=0
+def modify_response(stock_percent_list, strategy_name):
+    print("strategy_name")
+    print(strategy_name)
+    sum = 0
     for name in stock_percent_list:
         print(name)
-        sum=sum+name[0]
+        sum = sum + name[0]
 
     print("sum")
     print(sum)
-    response = {"strategy": name,
-    "sumPriorityScore":sum,"stock":[{"ticker": name[0],
-    "priorityScore": name[1]} for name in stock_percent_list]}
+    response = {"strategy": strategy_name,
+                "sumPriorityScore": sum, "stock": [{"ticker": name[0],
+                                                    "priorityScore": name[1]} for name in stock_percent_list]}
 
     print(response)
     return response
 
 
 stock_suggestions = {
-        'Ethical': ('GILD', 'GOOGL', 'NOV', 'PX', 'QCOM'),
-        'Growth': ('BIIB', 'AKRX', 'PSXP', 'IPGP', 'NFLX'),
-        'Index': ('LNDC', 'LWAY', 'MDLZ', 'RAVE', 'RIBT'),
-        'Quality': ('JNJ', 'KO', 'REGN', 'PEP', 'NKE'),
-        'Value': ('ALB', 'VIAC', 'BTI', 'CVS', 'AZO','VZ','ALXN'),
-    }
+    'Ethical': ('GILD', 'GOOGL', 'NOV', 'PX', 'QCOM'),
+    'Growth': ('BIIB', 'AKRX', 'PSXP', 'IPGP', 'NFLX'),
+    'Index': ('LNDC', 'LWAY', 'MDLZ', 'RAVE', 'RIBT'),
+    'Quality': ('JNJ', 'KO', 'REGN', 'PEP', 'NKE'),
+    'Value': ('ALB', 'VIAC', 'BTI', 'CVS', 'AZO', 'VZ', 'ALXN'),
+}
 
 
 def get_stock_list(strategy):
     print("strategy.....")
     # print(strategy)
     # print(strategy_ratio)
-    #define the stocks for each strategy
+    # define the stocks for each strategy
     stocks = stock_suggestions[strategy]
     # print(stocks) 
     annaul_gain = [(float(get_change(get_52_week_gain(name))), name) for name in stocks]
     month_gain = [(float(get_month(name)), name) for name in stocks]
     stock_priority = []
-    for x,y in zip(annaul_gain, month_gain):
+    for x, y in zip(annaul_gain, month_gain):
         # print("x y")
         # print(x[0])
         # print(y[0])
-        stock_priority.append((0.6*x[0] +0.4*y[0],x[1]))
-       
-    #sort
-    stock_priority.sort(key=lambda x:x[0],reverse=True)
+        stock_priority.append((0.6 * x[0] + 0.4 * y[0], x[1]))
+
+    # sort
+    stock_priority.sort(key=lambda x: x[0], reverse=True)
     print(stock_priority)
-   
+
     return stock_priority[:3]
 
 
@@ -102,7 +98,7 @@ def get_52_week_gain(stock_symbol):
     print("stock_symbol")
     print(stock_symbol)
     session = requests.session()
-    url = "https://financialmodelingprep.com/api/v3/enterprise-value/"+stock_symbol+"?period=quarter"
+    url = "https://financialmodelingprep.com/api/v3/enterprise-value/" + stock_symbol + "?period=quarter"
     # print(url)
     response = session.get(url, timeout=15)
     return response
@@ -111,14 +107,14 @@ def get_52_week_gain(stock_symbol):
 def get_change(data):
     # print("data")
     # print(data.json())
-    try: 
+    try:
         stock_data = data.json()
         # print(stock_data)
     except ValueError:
-        tempData = { 'error_msg' :'Deserialization Fails.'}
+        tempData = {'error_msg': 'Deserialization Fails.'}
         return tempData
 
     open = float(stock_data['enterpriseValues'][1]['Stock Price'])
-    close= float(stock_data['enterpriseValues'][0]['Stock Price'])
-    change=close-open
+    close = float(stock_data['enterpriseValues'][0]['Stock Price'])
+    change = close - open
     return change
